3167c2563dafcf35b2b0dbc989e45af727f21f60
touzgar <ghaithslama115@gmail.com>
2026-06-23 04:14:00 +0100

    Fix: Add missing component exports and fix .gitignore

---
+++ frontend/src/components/public/index.ts
@@ -0,0 +1,6 @@
+export { Navigation } from './Navigation';
+export { Footer } from './Footer';
+export { default as ProductGrid } from './ProductGrid';
+export { default as ProductCard } from './ProductCard';
+export { default as ProductImageGrid } from './ProductImageGrid';
+export { default as ProductTable } from './ProductTable';
